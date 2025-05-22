b3e1317588827d94b2bd019cedde0ea329566c2b
Armin <armink.gta@gmail.com>
2025-05-21 21:04:12 -0400

    working

--- src/security-service.ts
+++ src/security-service.ts
@@ -5,33 +5,37 @@ const JWKS_URL = process.env.COGNITO_JWKS!;
 
 export const handler = async (event: any, _context: any, callback: any) => {
 
+    console.log(`Authorizer Logging`);
     try {
+        console.log(`getting Auth Header`);
         const authHeader = event.identitySource?.[0];
         if (!authHeader) {
             return callback("Unauthorized");
         }
-
+        console.log(`got Auth Header :: ${JSON.stringify(authHeader)}`);
 
         const token = authHeader.split(' ')[1];
+        console.log(`got token :: ${JSON.stringify(token)}`);
         if (!token) {
             return callback("Unauthorized");
         }
-
+        console.log(`decoding`);
         const decodedHeader = jwt.decode(token, { complete: true })?.header as jwt.JwtHeader;
         if (!decodedHeader || !decodedHeader.kid) {
             return callback("Unauthorized");
         }
-
+        console.log(`decoded :: ${JSON.stringify(decodedHeader)}`);
         const jwks = (await axios.get(JWKS_URL)).data.keys;
 
         const key = jwks.find((k: any) => k.kid === decodedHeader.kid);
         if (!key) {
             return callback("Unauthorized");
         }
-
+        console.log(`got key :: ${JSON.stringify(key)}`);
         const pubKey = buildPem(key.n, key.e);
+        console.log(`got pubKey :: ${JSON.stringify(pubKey)}`);
         const verifiedToken = jwt.verify(token, pubKey, { algorithms: ['RS256'] }) as any;
-
+        console.log(`verified token :: ${JSON.stringify(verifiedToken)}`);
         callback(null, generatePolicy(verifiedToken.sub, 'Allow', event.routeArn, { sub: verifiedToken.sub }));
 
     } catch (error) {
